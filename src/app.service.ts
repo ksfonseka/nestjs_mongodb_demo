ff141e986b9e14cdc8531e02b61e20224e7a3825
ksfonseka <95290398+ksfonseka@users.noreply.github.com>
2023-12-01 20:20:18 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! sdf dsfdfg';
+    return 'Hello World! sdf ';
   }
 }
